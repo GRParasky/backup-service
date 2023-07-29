7a448df0bbca6183a59ced3f6e23e4312461ca84
grparasky <grparasky@gmail.com>
2023-07-29 20:52:32 -0300

    Importando funções e corrigindo chamadas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog as fs
 
+import functions as f
+
 
 file_path = str()
 
@@ -12,8 +14,8 @@ root.geometry(f"{app_width}x{app_height}")
 root.title("Backup FW7")
 
 label = tk.Label(root, text="Bem-vindo ao serviço de Backup do Seviço da FW!")
-service_path = tk.Button(root, text="Caminho do Integrador", command=service_folder)
-exit_button = tk.Button(root, text="Sair", command=exit_program, padx=30)
+service_path = tk.Button(root, text="Caminho do Integrador", command=f.service_folder)
+exit_button = tk.Button(root, text="Sair", command=f.exit_program, padx=30)
 
 label.pack()
 service_path.pack()
