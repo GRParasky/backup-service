2dcd8715467bc64cded71d07634415540096e5f3
grparasky <grparasky@gmail.com>
2023-07-29 21:26:09 -0300

    Movendo funções para o arquivo principal novamente

--- functions.py
+++
@@ -1,8 +0,0 @@
-from tkinter import filedialog as fs
-
-def service_folder():
-    file_path = fs.askdirectory()
-    print(file_path)
-
-def exit_program():
-    exit()
--- main.py
+++ main.py
@@ -3,7 +3,18 @@ from tkinter import filedialog as fs
 
 import functions as f
 
+def service_folder():
+    file_path = fs.askdirectory()
+    print(file_path)
 
+def exit_program():
+    exit()
+
+def get_company():
+    company = input()
+    
+
+company = str()
 file_path = str()
 
 
@@ -14,13 +25,14 @@ root.geometry(f"{app_width}x{app_height}")
 root.title("Backup FW7")
 
 label = tk.Label(root, text="Bem-vindo ao serviço de Backup do Seviço da FW!")
+
 service_path = tk.Button(root, text="Caminho do Integrador", command=f.service_folder)
 exit_button = tk.Button(root, text="Sair", command=f.exit_program, padx=30)
 
 label.pack()
 service_path.pack()
-exit_button.pack(side=tk.BOTTOM, )
+exit_button.pack(side=tk.BOTTOM)
 
-print(file_path)
+print(service_path.__str__())
 
 root.mainloop()
